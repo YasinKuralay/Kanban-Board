2d2f3b29553e9c8da2eb15d8151579c77e413b76
Yasin Suat Kuralay <ysk@Yasins-Air.fritz.box>
2023-12-22 15:07:35 +0100

    First commit.

---
+++ src/app/ui/mummy/mummy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mummy',
+  standalone: true,
+  imports: [],
+  templateUrl: './mummy.component.html',
+  styleUrl: './mummy.component.scss'
+})
+export class MummyComponent {
+
+}
