a5fd14e249caba1344a425f616dbbb369d8a4286
yasinkuralay <yasinkuralay@gmail.com>
2024-06-01 12:42:44 +0000

    #2 Header component intro: Visuals of Mobile done.

---
+++ src/app/ui/header/header.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { HeaderComponent } from './header.component';
+
+const meta: Meta<HeaderComponent> = {
+    title: 'UI/Header',
+    component: HeaderComponent,
+};
+
+export default meta;
+type Story = StoryObj<HeaderComponent>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  render: () => ({
+    // props: {
+    // },
+  }),
+};
