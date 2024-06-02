4e9f73ebcff98f31f8c6110ba6951d40102d8975
Yasin Kuralay <yasinkuralay@github.com>
2024-06-02 15:03:14 +0200

    #4 Implemented accessibility prerequisites for launching a dialog in the mobile header.

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -1,10 +1,30 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public mobileBoardsDialogIsOpen = false;
+
+  /**
+   * Opens the "Boards Dialog" on the mobile header.
+   *
+   * @remarks
+   * This dialog can only be launched on the mobile header. The dialog will automatically be closed if the user extends the Viewport-Width over 767px.
+   * This method launches an Angular CDK dialog that displays a list of boards. The Dialog additionally includes a light-dark theme toggle.
+   * For further reference on the CDK, please visit: https://material.angular.io/cdk/dialog/overview
+   *
+   */
+  openMobileBoardsDialog() {
+    this.mobileBoardsDialogIsOpen = !this.mobileBoardsDialogIsOpen;
+    console.log(
+      'openMobileBoardsDialog state: ',
+      this.mobileBoardsDialogIsOpen
+    );
+  }
+}
