55194cfda44cad8185e066f5af2b3ce3727ba733
Yasin Kuralay <yasinkuralay@github.com>
2024-06-22 20:37:28 +0200

    #4 Implemented the styling and rendering logic of the mobile-boards-dialog. Functionality to switch boards and the theme-switcher is still open.

--- src/app/ui/dialogs/mobile-boards-dialog/mobile-boards-dialog.component.ts
+++ src/app/ui/dialogs/mobile-boards-dialog/mobile-boards-dialog.component.ts
@@ -1,16 +1,44 @@
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { SvgLoaderComponent } from '../../svg-loader/svg-loader.component';
+import { Subscription } from 'rxjs';
+import { BoardsService } from '../../../data-layer/boards.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-mobile-boards-dialog',
   standalone: true,
-  imports: [],
+  imports: [NgClass, SvgLoaderComponent],
   templateUrl: './mobile-boards-dialog.component.html',
   styleUrl: './mobile-boards-dialog.component.scss',
+  encapsulation: ViewEncapsulation.None, // Encapsulation is set to none to allow for the dialog to apply its styles to its children.
 })
-export class MobileBoardsDialogComponent {
+export class MobileBoardsDialogComponent implements OnDestroy {
+  private selectedBoardIDSubscription: Subscription;
+  public selectedBoardID: number | undefined;
+
+  private boardNamesSubscription: Subscription;
+  public boardNames: string[] | undefined;
+
   constructor(
     public dialogRef: DialogRef<string>,
     @Inject(DIALOG_DATA) public data: any,
-  ) {}
+    private boardsService: BoardsService,
+  ) {
+    this.selectedBoardIDSubscription =
+      this.boardsService.selectedBoardID$.subscribe((boardID) => {
+        this.selectedBoardID = boardID;
+      });
+
+    this.boardNamesSubscription = this.boardsService.boardNames$.subscribe(
+      (boardNames) => {
+        this.boardNames = boardNames;
+      },
+    );
+  }
+
+  ngOnDestroy() {
+    this.selectedBoardIDSubscription.unsubscribe();
+    this.boardNamesSubscription.unsubscribe();
+  }
 }
