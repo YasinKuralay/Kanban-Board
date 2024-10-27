30de565d29656293c72195a92ae856a2b0874367
Yasin Kuralay <yasinkuralay@github.com>
2024-10-27 12:33:40 +0100

    #18 Dropdown Component functionality done: focusout event and tabbing now works. Styling will be done next.

--- src/app/ui/form/dropdown/dropdown.component.ts
+++ src/app/ui/form/dropdown/dropdown.component.ts
@@ -31,15 +31,31 @@ export class DropdownComponent implements OnDestroy {
    * Keeps track of whether the dropdown-list is open or closed.
    */
   isOpen = false;
+
+  /**
+   * The Popper.js instance that manages the dropdown-list positioning.
+   */
   popperInstance: any;
 
   constructor(private cdRef: ChangeDetectorRef) {}
 
+  /**
+   * The popper.js instance should be destroyed without ngOnDestroy anyways, but making 100% sure it's destroyed.
+   */
   ngOnDestroy() {
     this.destroyPopperInstance();
   }
 
-  createPopperInstance() {
+  /**
+   * Creates a new Popper.js instance to manage the dropdown-list positioning.
+   * The dropdown-list is positioned below the dropdown-header by default.
+   * If there is not enough space below, the dropdown-list is flipped to the top.
+   * This is done by the 'flip' modifier.
+   * The Popper.js instance is stored in the 'popperInstance' property.
+   * The instance is destroyed when the dropdown is closed.
+   *
+   */
+  private createPopperInstance() {
     if (this.dropdownHeader && this.dropdownList) {
       this.popperInstance = createPopper(
         this.dropdownHeader.nativeElement,
@@ -59,14 +75,25 @@ export class DropdownComponent implements OnDestroy {
     }
   }
 
-  destroyPopperInstance() {
+  /**
+   * Destroys the Popper.js instance if it exists.
+   */
+  private destroyPopperInstance() {
     if (this.popperInstance) {
       this.popperInstance.destroy();
       this.popperInstance = null;
     }
   }
 
-  toggleDropdown() {
+  /**
+   * Toggles the dropdown-list visibility via the isOpen property.
+   * If the dropdown-list is opened, the Popper.js instance is created.
+   * If the dropdown-list is closed, the Popper.js instance is destroyed.
+   *
+   * @remarks
+   * The change detection is triggered because if not, the @ViewChild properties are not updated before this function is finished executing.
+   */
+  public toggleDropdown() {
     this.isOpen = !this.isOpen;
     if (this.isOpen) {
       this.cdRef.detectChanges();
@@ -76,9 +103,50 @@ export class DropdownComponent implements OnDestroy {
     }
   }
 
-  selectOption(index: number) {
+  /**
+   * Handles the Space and Enter keydown events on the dropdown-header.
+   * Simply calls the toggleDropdown method.
+   */
+  public handleKeydownOnHeader(event: KeyboardEvent) {
+    if (event.key === ' ' || event.key === 'Enter') {
+      this.toggleDropdown();
+      event.preventDefault();
+    }
+  }
+
+  /**
+   * Handles the Space and Enter keydown events on the dropdown-list items.
+   * Selects the option at the given index, by calling the selectOption method with the index.
+   */
+  public handleKeydownOnListItem(event: KeyboardEvent, idx: number) {
+    if (event.key === ' ' || event.key === 'Enter') {
+      this.selectOption(idx);
+      event.preventDefault();
+    }
+  }
+
+  /**
+   * Selects the option at the given index.
+   * Emits the selectedOptionIndexChange event with the index.
+   * Closes the dropdown-list by setting the isOpen property to false.
+   */
+  public selectOption(index: number) {
     this.selectedOptionIndex = index;
     this.selectedOptionIndexChange.emit(index);
     this.isOpen = false;
+  }
+
+  /**
+   * Handles the focusout event on the dropdown-list:
+   * If the related target is not a child of the dropdown-list, the dropdown-list is closed.
+   *
+   * @remarks
+   * Since focusout events bubble, the child elements of the dropdown-list will trigger this event.
+   */
+  public handleFocusOutOnList(event: FocusEvent) {
+    // The event.relatedTarget (in FocusEvents) is the element that is gaining focus.
+    if (!this.dropdownList?.nativeElement.contains(event.relatedTarget)) {
+      this.isOpen = false;
+    }
   }
 }
